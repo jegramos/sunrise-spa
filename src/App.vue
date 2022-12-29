d940046a171dd937fc3faa841e5a1ddba3f2da6a
jegramos <jegramos.pa@gmail.com>
2022-12-29 15:54:35 +0800

    :test_tube: test

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import MenuBar from './components/MenuBar.vue'
+</script>
+
+<template>
+  <menu-bar></menu-bar>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// const app = createApp(App)
+
+import { vueApp } from "./app";
+vueApp.mount("#app");
